2c07517e50196412e4acaaf81ea4dcf2b924f0fe
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2024-02-29 03:48:08 +0100

    Crud product/_id

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -2,6 +2,7 @@
 const express= require ("express") //requiero express de la raiz index.js
 
 const router=express.Router(); //inicializo enrutador de express
+const Product = require("../models/Product.js");
 
 let Products = [//listado de productos ejemplos para que aparezcan en localHost//3005 de forma estructurada
     {  nombre: "Camiseta azúl", descripción:"ropa", imagen:"./",categoria:"camisetas", talla:"S",precio:4.50 },
@@ -23,6 +24,21 @@ router.post("/dashboard",(req,res)=>{
         precio:req.body.precio 
     }
     Products.push(nuevoProducto)
+})
+
+//GET /products/:productId
+router.get("/products:productId",async (req,res)=>{//Ruta Read products id
+
+    try {
+        const product = await Product.findById(req.params._id);//_id bajo se utiliza porqué lo especifica así mongo
+        res.send(product);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({
+            message: "There was a problem with the product with _id number: " +
+                req.params._id,
+        });
+    }
 })
 
 module.exports = {router}; //exporto el enrutador importante hay que exportar y importar de la misma forma
